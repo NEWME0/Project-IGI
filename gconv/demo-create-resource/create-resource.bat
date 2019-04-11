ce5f097f9df03d8ef5ec9670c8c7b391b67ba33a
Rotari Artiom <ordersone@gmail.com>
2019-04-11 03:10:22 +0300

    added demo-extract-resource

---
+++ gconv/demo-create-resource/create-resource.bat
@@ -0,0 +1,7 @@
+:: -InputPath=
+:: -OutputPath=
+:: -All
+
+..\bin\gconv.exe create-resource.qsc -InputPath=export/ -OutputPath=output/
+
+pause
---
+++ gconv/demo-extract-resource/extract-resource.bat
@@ -0,0 +1,7 @@
+:: -InputPath=
+:: -OutputPath=
+:: -All
+
+..\bin\gconv.exe extract-resource.qsc -InputPath=export/ -OutputPath=output/
+
+pause
