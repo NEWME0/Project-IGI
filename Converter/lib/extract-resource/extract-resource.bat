24eb014f2e999c1f7b66929b1dbc75ee9a4e3e7d
Rotari Artiom <ordersone@gmail.com>
2020-01-16 01:14:50 +0300

    Sly

--- Converter/lib/extract-resource/extract-resource.bat
+++ Converter/lib/extract-resource/extract-resource.bat
@@ -1,1 +1,2 @@
 ..\..\bin\gconv.exe extract-resource.qsc -InputPath=input\ -OutputPath=output\
+pause
---
+++ Converter/lib/make-tga/make-tga.bat
@@ -0,0 +1,2 @@
+..\..\bin\gconv.exe make-tga.qsc -InputPath=input\ -OutputPath=output\ -ConversionMode=ARGB32
+pause
