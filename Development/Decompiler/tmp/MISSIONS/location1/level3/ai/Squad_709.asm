f571aa2de44a3eb6bc2e8c09dcb1331a0e805366
Rotari Artiom <ordersone@gmail.com>
2020-01-31 03:02:19 +0300

    Removed Decompiler tmp

--- Development/Decompiler/tmp/MISSIONS/location1/level3/ai/Squad_709.asm
+++
@@ -1,5 +0,0 @@
-0          PUSHIIB    AISquad_DefaultHandler
-2          CALL       []
-7          BRA        12
-12         POP        
-13         BRK        
--- Development/Decompiler/tmp/MISSIONS/location3/LEVEL4/ai/534.asm
+++
@@ -1,5 +0,0 @@
-0          PUSHIIB    AIFunction_DefaultHandler
-2          CALL       []
-7          BRA        12
-12         POP        
-13         BRK        
