53ffad773c106325b2df28ab33f77cb6853eb2c2
Rotari Artiom <ordersone@gmail.com>
2020-02-19 17:09:16 +0300

    Added texconv

--- Development/texconv/__main__.py
+++ Development/texconv/__main__.py
@@ -6,38 +6,6 @@ import collections
 import tex
 
 
-def check(args):
-	count = 0
-	dataset = collections.defaultdict(set)
-
-	for root, dirs, files in os.walk(args.src):
-		for fn in files:
-			if not fn.endswith('.tex'):
-				continue
-
-			srcpath = os.path.join(root, fn)
-			texfile = tex.fromfile(srcpath)
-
-			dataset['signature'].add(texfile.signature)
-			dataset['_0'].add(texfile._0)
-			dataset['_1'].add(texfile._1)
-			dataset['_2'].add(texfile._2)
-			dataset['_3'].add(texfile._3)
-			dataset['_4'].add(texfile._4)
-			dataset['entry_x'].add(texfile.entry_x)
-			dataset['entry_y'].add(texfile.entry_y)
-			dataset['width'].add(texfile.width)
-			dataset['height'].add(texfile.height)
-			dataset['depth'].add(texfile.depth)
-
-			count += 1
-
-	for key, value in dataset.items():
-		print(key, value)
-
-	print('Checked: {0}'.format(count))
-
-
 def version(args):
 	count = 0
 
@@ -45,13 +13,15 @@ def version(args):
 
 	for root, dirs, files in os.walk(args.src):
 		for fn in files:
-			if not fn[-4:] in ('.tex', '.spr'):
+			if not fn[-4:] in ('.tex', '.spr', '.pic'):
 				continue
 
 			srcpath = os.path.join(root, fn)
+			print(srcpath)
 			texfile = tex.fromfile(srcpath)
 
-			dataset[texfile._0] += 1
+			dataset[texfile.version] += 1
+
 			count += 1
 
 	for key, value in dataset.items():
@@ -65,10 +35,6 @@ def main():
 
 	subparsers = parser.add_subparsers()
 	subparsers.require = True
-
-	che_parser = subparsers.add_parser('check', help="")
-	che_parser.add_argument('src', default='./_input/', help="")
-	che_parser.set_defaults(func=check)
 
 	ver_parser = subparsers.add_parser('version', help="")
 	ver_parser.add_argument('src', default='./_input/', help="")
--- Development/texconv/tex.py
+++ Development/texconv/tex.py
@@ -1,90 +1,350 @@
 import struct
 
 
-class TEX2:
+DEPTH = {
+	2:  2,
+	3:  4,
+	67: 4,
+}
+
+
+class Item06:
 	__slots__ = (
-		'signature',
-		'version',
+		'_0',
+		'_1',
+		'_2',
+		'_3',
 	)
 
 	@classmethod
-	def fromfile(cls, file):
-		tex = cls()
-		return tex
+	def fromfile(cls, file, count=1):
+		items = list()
+
+		for i in range(count):
+			obj = cls()
+
+			(
+				obj._0,
+				obj._1,
+				obj._2,
+				obj._3,
+			) = struct.unpack('4I', file.read(16))
+
+			items.append(obj)
+
+		return items
 
 
-
-class TEX7:
+class Item07:
 	__slots__ = (
-		'signature',
-		'version',
+		'offset',
+		'width_line',
+		'width',
+		'_0',
+		'height',
+		'reserved',
 	)
 
 	@classmethod
-	def fromfile(cls, file):
-		tex = cls()
-		return tex
+	def fromfile(cls, file, count=1):
+		items = list()
+
+		for i in range(count):
+			obj = cls()
+
+			(
+				obj.offset,
+				obj.width_line,
+				obj.width,
+				obj._0,
+				obj.height,
+				*obj.reserved,
+			) = struct.unpack('2I3H26B', file.read(40))
+
+			items.append(obj)
+
+		return items
 
 
-
-class TEX9:
+class Item09:
 	__slots__ = (
-		'signature',
-		'version',
+		'offset',
+		'mode',
+		'width_line',
+		'width',
+		'height',
+		'_0',
+		'reserved',
 	)
 
 	@classmethod
-	def fromfile(cls, file):
-		tex = cls()
-		return tex
+	def fromfile(cls, file, count=1):
+		items = list()
+
+		for i in range(count):
+			obj = cls()
+
+			(
+				obj.offset,
+				obj.mode,
+				obj.width_line,
+				obj.width,
+				obj.height,
+				obj._0,
+				*obj.reserved,
+			) = struct.unpack('2I4H16B', file.read(32))
+
+			items.append(obj)
+
+		return items
 
 
 
-class TEX11:
+class Loop02:
 	__slots__ = (
 		'signature',
 		'version',
 		'_1',
 		'_2',
-		'_3',
-		'_4',
-		'entry_x',
-		'entry_y',
+		'width_line',
 		'width',
 		'height',
 		'depth',
-
+		'bitmaps',
 	)
 
 	@classmethod
 	def fromfile(cls, file):
-		tex = cls()
-		return tex
+		obj = cls()
+
+		(
+			obj.signature,
+			obj.version,
+			obj._1,
+			obj._2,
+			obj.width_line,
+			obj.width,
+			obj.height,
+			obj.depth,
+		) = struct.unpack('4s3I4H', file.read(24))
+
+		obj.bitmaps = [file.read(obj.width * obj.height * obj.depth) for i in range(1)]
+
+		assert not file.read(), "Expected end of file"
+
+		return obj
+
+
+class Loop06:
+	__slots__ = (
+		'signature',
+		'version',
+		'_0',
+		'_1',
+		'count',
+		'_2',
+		'items',
+	)
+
+	@classmethod
+	def fromfile(cls, file):
+		obj = cls()
+
+		(
+			obj.signature,
+			obj.version,
+			obj._0,
+			obj._1,
+			obj.count,
+			obj._2,
+		) = struct.unpack('4s5I', file.read(24))
+
+		obj.items = Item06.fromfile(file, obj.count)
+
+		return obj
+
+
+class Loop07:
+	__slots__ = (
+		'signature',
+		'version',
+		'_0',
+		'_1',
+		'_2',
+		'_3',
+		'_4',
+		'offset',
+		'count',
+		'_5',
+		'width',
+		'height',
+		'mode',
+		'items',
+		'bitmaps',
+		'loop6',
+	)
+
+	@classmethod
+	def fromfile(cls, file):
+		obj = cls()
+
+		(
+			obj.signature,
+			obj.version,
+			obj._0,
+			obj._1,
+			obj._2,
+			obj._3,
+			obj._4,
+			obj.offset,
+			obj.count,
+			obj._5,
+			obj.width,
+			obj.height,
+			obj.mode,
+		) = struct.unpack('4s12I', file.read(52))
+
+		obj.items = Item07.fromfile(file, obj.count)
+		obj.bitmaps = [file.read(obj.width * obj.height * DEPTH[obj.mode]) for i in obj.items]
+		obj.loop6 = Loop06.fromfile(file)
+
+		assert not file.read(), "Expected end of file"
+
+		return obj
+
+
+class Loop09:
+	__slots__ = (
+		'signature',
+		'version',
+		'_0',
+		'_1',
+		'_2',
+		'_3',
+		'_4',
+		'offset',
+		'count',
+		'_5',
+		'width',
+		'height',
+		'mode',
+		'items',
+		'bitmaps',
+		'loop6',
+	)
+
+	@classmethod
+	def fromfile(cls, file):
+		obj = cls()
+
+		(
+			obj.signature,
+			obj.version,
+			obj._0,
+			obj._1,
+			obj._2,
+			obj._3,
+			obj._4,
+			obj.offset,
+			obj.count,
+			obj._5,
+			obj.width,
+			obj.height,
+			obj.mode,
+		) = struct.unpack('4s12I', file.read(52))
+
+		obj.items = Item09.fromfile(file, obj.count)
+		obj.bitmaps = [file.read(obj.width * obj.height * DEPTH[obj.mode]) for i in obj.items]
+		obj.loop6 = Loop06.fromfile(file)
+
+		assert not file.read(), "Expected end of file"
+
+		return obj
+
+
+class Loop11:
+	__slots__ = (
+		'signature',
+		'version',
+		'mode',
+		'multi',
+		'_0',
+		'_1',
+		'_2',
+		'_3',
+		'width',
+		'height',
+		'depth',
+		'bitmaps'
+	)
+
+	@classmethod
+	def fromfile(cls, file):
+		obj = cls()
+
+		(
+			obj.signature,
+			obj.version,
+			obj.mode,
+			obj.multi,
+			obj._0,
+			obj._1,
+			obj._2,
+			obj._3,
+			obj.width,
+			obj.height,
+			obj.depth,
+		) = struct.unpack('4s4I6H', file.read(32))
+
+		obj.bitmaps = list()
+		lod = 0
+
+		while True:
+			w = obj.width // (1 << lod)
+			h = obj.height // (1 << lod)
+			d = DEPTH[obj.mode]
+			s = w * h * d
+			d = file.read(s)
+
+			if len(d) == 0:
+				break
+
+			obj.bitmaps.append(d)
+			lod += 1
+
+
+		print(len(obj.bitmaps))
+
+		rest = file.read()
+		try:
+			assert not rest, "Expected end of file"
+		except:
+			print(rest)
+			raise
+
+		return obj
 
 
 
-def fromfile(srcfile):
-	if isinstance(srcfile, str):
-		srcfile = open(srcfile, 'rb')
+def fromfile(file):
+	if isinstance(file, str):
+		file = open(file, 'rb')
 
-	signature, version = struct.unpack('4sI', srcfile.read(8))
-	srcfile.seek(0, 0)
+	signature, version = struct.unpack('4sI', file.read(8))
 
 	assert signature == b'LOOP', "Unexpected signature"
 	assert version in (2, 7, 9, 11), "Unexpected version"
 
+	file.seek(0, 0)
+
+	if version == 2:
+		return Loop02.fromfile(file)
+
+	if version == 7:
+		return Loop07.fromfile(file)
+
+	if version == 9:
+		return Loop09.fromfile(file)
+
 	if version == 11:
-		tex = TEX11.fromfile(srcfile)
-
-	elif version == 9:
-		tex = TEX9.fromfile(srcfile)
-
-	elif version == 7:
-		tex = TEX7.fromfile(srcfile)
-
-	elif version == 2:
-		tex = TEX2.fromfile(srcfile)
-
-	srcfile.close()
-
-	return tex
+		return Loop11.fromfile(file)
