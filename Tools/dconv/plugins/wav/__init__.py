a03fd80a194831d587ac90375a9201db44d0e7f3
Rotari Artiom <ordersone@gmail.com>
2020-02-26 01:25:11 +0300

    Sound tests

--- Tools/dconv/plugins/wav/__init__.py
+++ Tools/dconv/plugins/wav/__init__.py
@@ -1,4 +1,3 @@
-from . import check
 from . import convert
 
 
@@ -6,10 +5,6 @@ def register_parser(cli):
     cmd = cli.add_parser('wav')
     sub = cmd.add_subparsers()
     sub.require = True
-
-    sub_check = sub.add_parser('check')
-    sub_check.add_argument('src')
-    sub_check.set_defaults(func=check.func)
 
     sub_convert = sub.add_parser('convert')
     sub_convert.add_argument('src')
