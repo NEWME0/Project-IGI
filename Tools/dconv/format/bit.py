cebaf84f895b636318be955d5a789be5d29448bc
Rotari Artiom <ordersone@gmail.com>
2020-02-27 17:38:26 +0300

    Added terrain bit and Template terrain cmd

---
+++ Tools/dconv/format/bit.py
@@ -0,0 +1,47 @@
+import array
+import struct
+
+
+def mirror(b):
+	b = int(bin(b)[2:].zfill(8)[::-1], 2)
+	b = struct.pack('B', b)[0]
+	return b
+
+
+class BITSquare:
+	__slots__ = ('unk0', 'unk1', 'side', 'data')
+
+	def __init__(self, *args):
+		self.unk0, self.unk1, self.side, self.data = args
+
+
+class BIT:
+	__slots__ = ('squares')
+
+	def __init__(self):
+		self.squares = list()
+
+	def load(self, fp):
+		if isinstance(fp, str):
+			fp = open(fp, 'rb')
+
+		arr = array.array('I')
+		arr.frombytes(fp.read(256*12))
+
+		for i in range(0, 256*3, 3):
+			unk0 = arr[i]
+			unk1 = arr[i + 1]
+			side = arr[i + 2]
+
+			if side:
+				data = fp.read((side * side) // 8)
+				data = bytes([mirror(b) for b in data])
+				self.squares.append(BITSquare(unk0, unk1, side, data))
+
+		assert not fp.read(), "Expected end of file"
+
+		fp.close()
+
+
+	def save(self, fp):
+		NotImplemented
