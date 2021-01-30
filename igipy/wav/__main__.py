24b08b622393ab4962e8fcdcd45f9fd6054e8a36
Rotari Artiom <ordersone@gmail.com>
2021-01-30 19:48:26 +0200

    Added help strings

--- igipy/wav/__main__.py
+++ igipy/wav/__main__.py
@@ -9,13 +9,13 @@ from igipy.wav.converters import WAVFileConverter
 from igipy.wav.serializers import WAVFileSerializer
 
 
-app = Typer(add_completion=False, help='Not implemented')
+app = Typer(add_completion=False, help='Operations with IGI WAV files.')
 
 
-@app.command(help='Serializer file without any output')
+@app.command(help='Serialize IGI WAV file without any output.')
 def serialize(
-    src: Path = Argument(..., case_sensitive=False, help='Source file path'),
-    verbose: bool = Option(default=False, help='Show internal information.')
+    src: Path = Argument(..., case_sensitive=False, help='Source file path.'),
+    verbose: bool = Option(default=False, help='Internal information.')
 ):
     if not src.is_file():
         return echo(f'Source file ({src}) not found.')
@@ -24,7 +24,7 @@ def serialize(
     with process.manager():
         serializer = WAVFileSerializer()
 
-        with process.handler('Failed at serialization', ValidationError):
+        with process.handler('Failed at serialization.', ValidationError):
             with open(src, 'rb') as src_file:
                 instance = serializer.load(src_file)
                 process.results = instance.meta
@@ -35,13 +35,13 @@ def serialize(
         return echo(process.results)
 
 
-@app.command(help='Not implemented')
+@app.command(help='Convert IGI WAV file to Waveform.')
 def convert(
     src: Path = Argument(..., case_sensitive=False, help='Source file path.'),
     dst: Path = Argument(..., case_sensitive=False, help='Destination file path.'),
-    force: bool = Option(default=False, help="Force conversion"),
-    verbose: bool = Option(default=False, help="Show internal information."),
-    override: bool = Option(default=False, help="Override destination file if exists."),
+    force: bool = Option(default=False, help="Force conversion."),
+    verbose: bool = Option(default=False, help="Internal information."),
+    override: bool = Option(default=False, help="Override destination file."),
 ):
     if not src.is_file():
         return echo(f'Source file ({src}) not found.')
@@ -53,13 +53,13 @@ def convert(
     with process.manager():
         serializer = WAVFileSerializer(verbose=verbose)
 
-        with process.handler('Failed at serialization', ValidationError):
+        with process.handler('Failed at serialization.', ValidationError):
             with open(src, 'rb') as src_file:
                 instance = serializer.load(src_file)
 
         converter = WAVFileConverter(verbose=verbose, force=force)
 
-        with process.handler('Failed at conversion', ConversionError):
+        with process.handler('Failed at conversion.', ConversionError):
             with open(dst, 'wb') as dst_file:
                 converter.convert(instance, dst_file)
 
